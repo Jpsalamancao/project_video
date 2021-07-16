e7649e0aa517b0f64c2f52fb1905af829501c0b7
Juan Paulo Salamanca <jpsalamancao@gmail.com>
2021-07-15 22:15:41 -0500

    NOfunciona las imagenes

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../assets/styles/components/Carousel.scss';
+
+const Carousel = ({Children}) => (
+    <section className="carousel">
+    <div className="carousel__container">
+        {Children}
+    </div>
+    </section>
+);
+
+export default Carousel;
+
